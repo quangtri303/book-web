d1e14f432b21704cb8348fb00cc1ac25a1a1d584
quangtri303 <quangtri3032001@gmail.com>
2024-09-13 18:50:47 +0700

    feat: add top slider

--- src/components/Home/BannerPC.vue
+++ src/components/Home/BannerPC.vue
@@ -1,6 +1,22 @@
 <script setup>
 import { RouterLink } from 'vue-router';
+import { ref } from 'vue';
 import yinyang from "../../assets/icons/yinyang.svg"
+
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import { Pagination } from 'swiper/modules';
+
+const modules = [Pagination];
+
+const activeSlide = ref(0);
+const onSlideChange = (swiper) => {
+    activeSlide.value = swiper.realIndex;
+};
+
+
 const category = [
     {
         id: 1,
@@ -65,6 +81,35 @@ const news = [
         title: "Các đơn vị tiền tệ",
     }
 ]
+const imgSlide = [
+    {
+        id: 0,
+        url: "https://truyen.tangthuvien.vn/images/intro/ttv-translate-1.jpg",
+        name: "TTV Translate",
+    },
+    {
+        id: 1,
+        url: "https://truyen.tangthuvien.vn/images/slide7.jpg",
+        name: "Lâm Uyên Hành"
+    },
+    {
+        id: 2,
+        url: "https://truyen.tangthuvien.vn/images/slide9.jpg",
+        name: "Vạn Cổ Tối Cường Tông"
+    },
+    {
+        id: 3,
+        url: "https://truyen.tangthuvien.vn/images/slide8.jpg",
+        name: "Quỷ Bí Chi Chủ"
+    },
+    {
+        id: 4,
+        url: "https://truyen.tangthuvien.vn/images/slide3.jpg",
+        name: "Phàm Nhân Tu Tiên"
+    }
+]
+
+
 </script>
 
 <template lang="pug">
@@ -79,7 +124,27 @@ const news = [
                         p {{ item.name }}
                         p {{ item.amount }}
         .box
-            p slider
+            swiper(:modules="modules" class="mySwiper swiper" )
+                swiper-slide(v-for="item in imgSlide" :key="item.id")
+                    img(:src="item.url" alt="item.id" :id="item.id" )
+            .nav-button
+                a(v-for="item in imgSlide" :key="item.id" :href="`#${item.id}`") {{ item.name }}
+            .top-user.flex.flex-row.justify-between
+                .cover
+                    img.user(src="https://www.nae.vn/ttv/ttv/public/images_user/120722.png")
+                    .flag#no1
+                        p No.1
+                        p Converter
+                .cover
+                    img.user(src="https://www.nae.vn/ttv/ttv/public/images_user/120722.png")
+                    .flag#no2
+                        p No.1
+                        p Đại gia
+                .cover
+                    img.user(src="https://www.nae.vn/ttv/ttv/public/images_user/120722.png")
+                    .flag#no3
+                        p No.1
+                        p Tiêu phí
         .news
             .newstitle Tin tức
             ul.newsitem(v-for="item in news" :key="item.id")
@@ -108,7 +173,6 @@ const news = [
     grid-auto-rows: 60px
 
 
-
 .category-item a
     height 100%
     hover-text(black,red)
@@ -128,9 +192,71 @@ const news = [
     flex-direction: column
 
 .box
-    flex 1 1 55%
-    background black
+    width 700px
     margin 0px 10px
+    .swiper
+        height 300px
+        img
+            width 100%
+        .swiper-pagination
+            display flex
+            justify-content space-between
+    .nav-button
+        display flex
+        justify-content space-between
+        gap 0.5px
+        
+        a   
+            flex 1 2 20%
+            padding 10px 0px
+            text-align center
+            color white
+            background rgba(0, 0, 0, 0.7)
+            font-size 14px
+    .top-user
+        .cover
+            background-image: url("https://truyen.tangthuvien.vn/images/cover2.jpg");
+            background-size: cover
+            background-position: center
+            position relative
+            width 33%
+            height 130px
+            object-fit cover
+            overflow hidden
+            .user
+                width 80px
+                height 80px
+                border-radius 50%
+                position absolute
+                top 50%
+                left 50%
+                transform translate(-50%, -50%)
+                object-fit cover
+            .flag
+                position absolute
+                top 0
+                right 0px
+                transform rotate(45deg) translate(0px, -80px)
+                display flex
+                flex-direction column
+                justify-content end
+                align-items center
+                height 100px
+                width 100px
+                color white
+                padding 5px 10px
+                font-size 14px
+                font-weight bold
+                border-radius 5px
+                z-index 1
+            #no1
+                background red
+            #no2
+                background #327c86
+            #no3
+                background #a3af1e
+
+
 
 .news
     flex 1 1 20%
